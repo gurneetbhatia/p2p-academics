b348fef40c598672208e22ee8824b7ab38ce9e23
gurneetbhatia <sbgurneet@gmail.com>
2021-09-18 14:55:57 -0400

    Add form on registration page

--- src/components/register.js
+++ src/components/register.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './register.css';
 
 class RegisterPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        console.log(this.state.value);
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="register-container">
@@ -31,14 +49,19 @@ class RegisterPage extends React.Component {
                     </Carousel.Item>
 
                     <Carousel.Item interval={null}>
+                        <h1>Register</h1>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group className="mb-3" controlId="name">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control value={this.state.value} onChange={this.handleChange} placeholder="John Doe"></Form.Control>
+                            </Form.Group>
+                            <Button type="submit" variant="info">Submit</Button>
+                        </Form>
                         <img
                             id="carousel-img"
                             className="d-block w-100"
-                            src={"https://via.placeholder.com/"+window.innerWidth+"x"+window.innerHeight+".png/252728/FFFFFF?text=Register by entering your name"}
-                            alt=""/>
-                        <Carousel.Caption>
-                            <h3>Some about text</h3>
-                        </Carousel.Caption>
+                            src={"https://via.placeholder.com/"+window.innerWidth+"x"+window.innerHeight+".png/252728/FFFFFF?text=%20"}
+                            alt="About"/>
                     </Carousel.Item>
                 </Carousel>
             </div>
@@ -47,3 +70,19 @@ class RegisterPage extends React.Component {
 }
 
 export default RegisterPage
+
+
+{/* <Carousel.Item interval={null}>
+                        <img
+                            id="carousel-img"
+                            className="d-block w-100"
+                            src={"https://via.placeholder.com/"+window.innerWidth+"x"+window.innerHeight+".png/252728/FFFFFF?text=Register by entering your name"}
+                            alt=""/>
+                        <Carousel.Caption>
+                            <Form>
+                                <Form.Group className="mb-3" controlId="name">
+                                    <Form.Label></Form.Label>
+                                </Form.Group>
+                            </Form>
+                        </Carousel.Caption>
+                    </Carousel.Item> */}
