33149f899c1d9a046c7f8ddd8976bc49fd31320d
gurneetbhatia <sbgurneet@gmail.com>
2021-09-18 14:41:38 -0400

    Add carousel on registration page

--- src/components/register.js
+++ src/components/register.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import './register.css';
 
 class RegisterPage extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Registration page</h1>
+            <div className="register-container">
+                <Carousel>
+                    <Carousel.Item height="500" interval={5000}>
+                        <img
+                            id="carousel-img"
+                            className="d-block w-100"
+                            src={"https://via.placeholder.com/"+window.innerWidth+"x"+window.innerHeight+".png/252728/FFFFFF?text=Welcome"}
+                            alt="Welcome"/>
+                        <Carousel.Caption>
+                            <h3>Some welcome text</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+
+                    <Carousel.Item interval={15000}>
+                        <img
+                            id="carousel-img"
+                            className="d-block w-100"
+                            src={"https://via.placeholder.com/"+window.innerWidth+"x"+window.innerHeight+".png/252728/FFFFFF?text=About"}
+                            alt="About"/>
+                        <Carousel.Caption>
+                            <h3>Some about text</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+
+                    <Carousel.Item interval={null}>
+                        <img
+                            id="carousel-img"
+                            className="d-block w-100"
+                            src={"https://via.placeholder.com/"+window.innerWidth+"x"+window.innerHeight+".png/252728/FFFFFF?text=Register by entering your name"}
+                            alt=""/>
+                        <Carousel.Caption>
+                            <h3>Some about text</h3>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
             </div>
         )
     }
