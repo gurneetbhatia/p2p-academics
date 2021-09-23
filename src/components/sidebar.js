8f32dadb7a990bb516398c96d576da96928c6033
gurneetbhatia <sbgurneet@gmail.com>
2021-09-23 15:50:04 -0400

    Add icons and tooltips for sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook, faQuestionCircle, faCompass, faUserFriends, faComment, faCog } from '@fortawesome/free-solid-svg-icons';
 
 class Sidebar extends React.Component {
     render() {
@@ -7,22 +11,34 @@ class Sidebar extends React.Component {
             <Nav className='col-md-12 d-none d-md-block bg-dark sidebar'>
                 <div className="sidebar-sticky"></div>
                 <Nav.Item>
-                    <Nav.Link href='/'>Query</Nav.Link>
+                    <OverlayTrigger key="right" placement="right" overlay={<Tooltip>Run Query</Tooltip>}>
+                        <Nav.Link href='/'><FontAwesomeIcon icon={faQuestionCircle}/></Nav.Link>
+                    </OverlayTrigger>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href='/'>My Repository</Nav.Link>
+                    <OverlayTrigger key="right" placement="right" overlay={<Tooltip>My Repository</Tooltip>}>
+                        <Nav.Link href='/'><FontAwesomeIcon icon={faBook}/></Nav.Link>
+                    </OverlayTrigger>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href='/'>Network</Nav.Link>
+                    <OverlayTrigger key="right" placement="right" overlay={<Tooltip>Explore Repositories</Tooltip>}>
+                        <Nav.Link href='/'><FontAwesomeIcon icon={faCompass}/></Nav.Link>
+                    </OverlayTrigger>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href='/'>Find people</Nav.Link>
+                    <OverlayTrigger key="right" placement="right" overlay={<Tooltip>Find people</Tooltip>}>
+                        <Nav.Link href='/'><FontAwesomeIcon icon={faUserFriends}/></Nav.Link>
+                    </OverlayTrigger>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href='/'>Chats</Nav.Link>
+                    <OverlayTrigger key="right" placement="right" overlay={<Tooltip>Chats</Tooltip>}>
+                        <Nav.Link href='/'><FontAwesomeIcon icon={faComment}/></Nav.Link>
+                    </OverlayTrigger>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link href='/'>Settings</Nav.Link>
+                    <OverlayTrigger key="right" placement="right" overlay={<Tooltip>Settings</Tooltip>}>
+                        <Nav.Link href='/'><FontAwesomeIcon icon={faCog}/></Nav.Link>
+                    </OverlayTrigger>
                 </Nav.Item>
             </Nav>
         );
