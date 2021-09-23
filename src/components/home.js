9512855ae27fd927f1f7e37b90de59a17b468eb0
gurneetbhatia <sbgurneet@gmail.com>
2021-09-23 14:22:04 -0400

    Instantiate sidebar for homepage

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './home.css';
+import Sidebar from './sidebar';
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div className="home-container" height={window.innerHeight.toString()}>Home page</div>
+            <div className="home-container">
+                <Sidebar/>
+            </div>
         );
     }
 }
