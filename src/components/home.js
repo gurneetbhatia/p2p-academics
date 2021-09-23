c0f25401b6beac34094b1ae13a8596dabd324b37
gurneetbhatia <sbgurneet@gmail.com>
2021-09-23 15:06:30 -0400

    Add styling and sidebar content

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './home.css';
 import Sidebar from './sidebar';
+import { Container, Row, Col } from 'react-bootstrap';
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div className="home-container">
-                <Sidebar/>
-            </div>
+            <Container fluid>
+                <Row>
+                    <Col xs={2} id="sidebar-wrapper">
+                        <Sidebar className="sidebar"/>
+                    </Col>
+                    <Col xs={10} id="page-content-wrapper">
+                        this is a test
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
 
 class Sidebar extends React.Component {
     render() {
         return (
-            <div>sidebar</div>
+            <Nav className='col-md-12 d-none d-md-block bg-dark sidebar'>
+                <div className="sidebar-sticky"></div>
+                <Nav.Item>
+                    <Nav.Link href='/'>Query</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href='/'>My Repository</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href='/'>Network</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href='/'>Find people</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href='/'>Chats</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link href='/'>Settings</Nav.Link>
+                </Nav.Item>
+            </Nav>
         );
     }
 }
