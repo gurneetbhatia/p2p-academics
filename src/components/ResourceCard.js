aced7f2edb26087f0bf2df20ca5baf633c46849e
gurneetbhatia <sbgurneet@gmail.com>
2021-10-06 13:34:03 -0400

    Multiple resource cards rendered on screen

--- src/components/ResourceCard.js
+++ src/components/ResourceCard.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 class ResourceCard extends React.Component {
     render() {
         return (
-            <div>{this.props.title}</div>
+            <Card bg="secondary" key={0} text="white" style={{width: '18rem'}} className="mb-2">
+                {/* <Card.Header>Repository title</Card.Header> */}
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>{this.props.abstract}</Card.Text>
+                    <Card.Footer>{this.props.authors.join(' ')}</Card.Footer>
+                </Card.Body>
+            </Card>
         )
     }
 }
+
+export default ResourceCard
--- src/pages/repository-page.js
+++ src/pages/repository-page.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import ResourceCard from '../components/ResourceCard'
 
-
-let resourcesList;
 
 window.api.receive("return-repo-resources", (response) => {
-  resourcesList = response;
+    console.log("handle response here")
 });
 
 window.api.send("get-repo-resources", {});
-console.log(resourcesList);
+// console.log(resourcesList);
+let resourcesList = [
+    {
+        title: "title 1",
+        abstract: "abstract 1",
+        authors: ["author 1", "author 2"]
+    },
+    {
+        title: "title 2",
+        abstract: "abstract 1",
+        authors: ["author 1", "author 2"]
+    },
+    {
+        title: "title 3",
+        abstract: "abstract 1",
+        authors: ["author 1", "author 2"]
+    },
+]
 
 class RepositoryPage extends React.Component {
     render() {
         return (
-            <div>Repository Page</div>
+            <div>
+                {
+                    resourcesList.map((element, index) => {
+                        return <ResourceCard key={index} title={element.title} abstract={element.abstract} authors={element.authors}></ResourceCard>
+                    })
+                }
+                {/* <ResourceCard title="some text" abstract="some abstract" authors={["name 1", "name 2"]}></ResourceCard> */}
+            </div>
         );
     }
 }
