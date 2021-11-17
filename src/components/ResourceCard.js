7e31d979a33b003547433c6a2cf7f46e9895d298
gurneetbhatia <sbgurneet@gmail.com>
2021-11-17 16:24:04 -0500

    Add info button to resource card

--- src/components/ResourceCard.js
+++ src/components/ResourceCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faTrash, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 class ResourceCard extends React.Component {
     constructor(props) {
@@ -22,8 +22,9 @@ class ResourceCard extends React.Component {
                 {/* <Card.Header>Repository title</Card.Header> */}
                 <Card.Body>
                     <Card.Title>{this.props.title ? this.props.title : this.props.filename}</Card.Title>
-                    <Card.Text>{this.props.abstract ? this.props.abstract : "Please complete the details for this resource."}</Card.Text>
+                    <Card.Text>{this.props.abstract ? this.props.abstract : "Please complete the details for this resource (Click on the info icon below)."}</Card.Text>
                     <Card.Footer>
+                        <Button variant="info"><FontAwesomeIcon icon={faInfoCircle}></FontAwesomeIcon></Button>
                         <Button variant="info"><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></Button>
                         <Button variant="danger" onClick={this.handleDelete}><FontAwesomeIcon icon={faTrash}></FontAwesomeIcon></Button>
                     </Card.Footer>
