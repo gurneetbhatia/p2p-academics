dd58c74759707a41d9e54dc842f7d56033287e51
gurneetbhatia <sbgurneet@gmail.com>
2021-11-17 16:30:24 -0500

    Initialise resource info popup component

---
+++ src/components/resource-info-popup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Modal from 'react-bootstrap/Modal';
+
+class ResourceInfoPopup extends React.Component {
+    render () {
+        return (
+            <>
+                popup
+            </>
+        )
+    }
+}
+
+export default ResourceInfoPopup;
