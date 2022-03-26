0cb10e9657665f82824912e5c0e2c4e10d3d09f9
gurneetbhatia <sbgurneet@gmail.com>
2022-03-26 06:56:59 -0400

    Connect endpoints on frontend for profiles

--- src/pages/find-people-page.js
+++ src/pages/find-people-page.js
@@ -2,6 +2,24 @@ import React from 'react';
 import ProfileCard from '../components/profile-card';
 
 class FindPeoplePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profiles: []
+        };
+    }
+
+    componentWillMount() {
+        window.parseInt.receive("return-user-profiles", (response) => {
+            if (response) {
+                this.setState({profiles: response});
+                console.log(response);
+            }
+        });
+
+        window.api.send("get-user-profiles", {});
+    }
+
     render() {
         return (
             <div>
